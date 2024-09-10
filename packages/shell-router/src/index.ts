7a711df0c9d8b5744d48b21a861e83a182c02a99
kangactor123 <kangactor123@naver.com>
2024-09-10 21:35:10 +0900

    feat/shell 간 navigate hook 구현

--- packages/shell-router/src/index.ts
+++ packages/shell-router/src/index.ts
@@ -3,10 +3,14 @@ import { injectFactory } from "./injector";
 import AppRoutingManager from "./components/app-routing-manager";
 import { Auth0ClientProvider } from "./providers/auth0-client-provider";
 import useAuth0Client from "./hooks/use-auth0-client";
+import useShellNavigate from "./hooks/use-shell-navigate";
+import useShellNavigateListener from "./hooks/use-shell-navigator-listener";
 
 export {
   useShellEvent,
   useAuth0Client,
+  useShellNavigate,
+  useShellNavigateListener,
   injectFactory,
   AppRoutingManager,
   Auth0ClientProvider,
